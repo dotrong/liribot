4ed41f1ae23a99b87ff50389290d87a44bb4d764
dotrong <dohieu@gmail.com>
2017-05-22 20:55:38 -0700

    initial

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'XO2Jla5v4ivHWePtQwdMjcgoC',
+  consumer_secret: 'WBDs7JZ00ml1O4Q7IijgCimRa3kCVrzQ5JcRNWCUWyAe7bVehF',
+  access_token_key: '866854968208248833-YdMDclCiYuif0SAHvGjzGzL2xJHJJkj',
+  access_token_secret: 'nVC16UwIsh50YFHskZox7o8epkEVd5xeNVcA68nRC08to',
+}
